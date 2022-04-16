feb2cdd5d13f1377bf3189b8a97fe90dfedded87
si-shakil <sishakil850@gmail.com>
2022-04-16 11:21:05 +0600

    banner added

--- src/Components/Home/Banner/Banner.js
+++ src/Components/Home/Banner/Banner.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./Banner.css";
+import "../public.css";
 
 const Banner = () => {
-  return <div className="banner"></div>;
+  return (
+    <div className="banner">
+      <h1>Best food waiting for your belly</h1>
+
+      {/* search section */}
+      <div className="search-div">
+        <input type="text" name="" id="search" />{" "}
+        <input className="special-button" type="button" value="Search" />
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
