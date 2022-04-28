299d3dca587ca26488682ea9a0b4639d3861c1f2
si-shakil <sishakil850@gmail.com>
2022-04-28 13:04:06 +0600

    footer added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import { Navbar } from "react-bootstrap";
 import { Route, Routes } from "react-router-dom";
 import Breakfast from "./Components/Home/Food/Breakfast/Breakfast";
 import Dinner from "./Components/Home/Food/Dinner/Dinner";
 import Lunch from "./Components/Home/Food/Lunch/Lunch";
+import Footer from "./Components/Home/Footer/Footer";
 import Home from "./Components/Home/Home";
 import Login from "./Components/User/Login/Login";
 import Register from "./Components/User/Register/Register";
@@ -18,6 +20,7 @@ function App() {
         <Route path="/login" element={<Login></Login>}></Route>
         <Route path="/register" element={<Register></Register>}></Route>
       </Routes>
+      <Footer></Footer>
     </div>
   );
 }
--- src/Components/Home/Footer/Footer.js
+++ src/Components/Home/Footer/Footer.js
@@ -1,12 +1,31 @@
 import React from "react";
+import Logo from "../../../images/logo.png";
+import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div className="footer">
-      <div className="first-column"></div>
-      <div className="second-column">
-        <div className="first-coloumn"></div>
-        <div className="second-column"></div>
+    <div className="footer  ">
+      <div className="container">
+        <div className="row">
+          <div className="col-6">
+            <img className="logo" src={Logo} alt="" />
+            <div className="copy">&copy; Red Onion Restaurent</div>
+          </div>
+          <div className="col-6 row">
+            <div className="col-6">
+              <p>About Us</p>
+              <p>Read Our Blog</p>
+              <p>Privacy Policy</p>
+              <p>Terms of Use</p>
+            </div>
+            <div className="col-6">
+              <p>Get Help</p>
+              <p>Read FAQs</p>
+              <p>Pricing</p>
+              <p>Restaurent Near Me</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
