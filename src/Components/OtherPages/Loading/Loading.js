a432e0ff4dca0b6b6e6dc1fa246331629d223fc8
si-shakil <sishakil850@gmail.com>
2022-05-06 16:57:01 +0600

    sign in by google and email,password applied

--- src/Components/OtherPages/Loading/Loading.js
+++ src/Components/OtherPages/Loading/Loading.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./Loading.css";
 
 const Loading = () => {
   return (
-    <div class="d-flex justify-content-center">
+    <div class="d-flex justify-content-center align-items-center loading">
       <div class="spinner-border" role="status">
         <span class="visually-hidden">Loading...</span>
       </div>
--- src/Components/User/Login/Login.js
+++ src/Components/User/Login/Login.js
@@ -1,20 +1,58 @@
 import React, { useRef } from "react";
 import Logo from "../../../images/logo2.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./Login.css";
 import google from "../../../images/icons/google.png";
 import facebook from "../../../images/icons/facebook.png";
 import twitter from "../../../images/icons/twitter.png";
+import {
+  useSignInWithEmailAndPassword,
+  useSignInWithGoogle,
+} from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
+import Loading from "../../OtherPages/Loading/Loading";
+import { toast } from "react-toastify";
 const Login = () => {
+  //to navigate the user
+  const navigate = useNavigate();
+  //to sign in by user email and password
+  const [
+    signInWithEmailAndPassword,
+    userOfEmail,
+    loadingOfEmail,
+    errorOfEmail,
+  ] = useSignInWithEmailAndPassword(auth);
+  //to sign in by google
+  const [signInWithGoogle, userOfGoogle, loadingOfGoogle, errorOfGoogle] =
+    useSignInWithGoogle(auth);
+  //to get the user email and password
   const emailRef = useRef("");
   const passwordRef = useRef("");
+  const signIn = (e) => {
+    e.preventDefault();
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    signInWithEmailAndPassword(email, password);
+  };
+  const signInByGoog = () => {
+    signInWithGoogle();
+  };
+  if (loadingOfEmail || loadingOfGoogle) {
+    return <Loading></Loading>;
+  }
+  if (errorOfEmail || errorOfGoogle) {
+    toast("Try again please...");
+  }
+  if (userOfEmail || userOfGoogle) {
+    navigate("/");
+  }
   return (
     <div className="register-form">
       <div className="w-25 mx-auto">
         <div className="logo">
           <img src={Logo} alt="" />
         </div>
-        <form>
+        <form onSubmit={signIn}>
           <input
             type="email"
             ref={emailRef}
@@ -42,7 +80,7 @@ const Login = () => {
           <div className="line w-50"></div>
         </div>
         <div className="social-login">
-          <button className="login-button w-100 mb-3">
+          <button onClick={signInByGoog} className="login-button w-100 mb-3">
             <img src={google} alt="" /> &nbsp; &nbsp; Sign In With Google
           </button>
           <button className="login-button w-100 mb-3">
